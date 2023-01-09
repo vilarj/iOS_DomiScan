9854b132a5853ede2acb1b086a4d5fdd41b9e7cc
Vilarj <lamaravillasan@outlook.com>
2023-01-09 11:45:58 -0500

    update

--- DomiScan/ViewController.swift
+++ DomiScan/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	@IBOutlet var header: UILabel! // DomiScan
+	
+	@IBOutlet var player1: UITextField!
+	
+	@IBOutlet var player2: UITextField!
+	
+	@IBOutlet var left_score_box: UITextView!
+	
+	@IBOutlet var right_score_box: UITextView!
+	
+	@IBOutlet var left_auto_assign: UITextField!
+	
+	@IBOutlet var right_auto_assign: UITextField!
+	
+	@IBOutlet var left_add_25: UITextField!
+	
+	@IBOutlet var right_add_25: UITextField!
+	
+	@IBOutlet var left_add_50: UITextField!
+	
+	@IBOutlet var right_add_50: UITextField!
+	
+	@IBAction func camera(_ sender: Any) {
+		
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	/**
+	 This function takes two parameters and then assign assign the value to the
+	 correct player.
+	 
+	@args:
+	 playerName: The name of the player whom the score will be added
+	 amount: The actual value to be added
+	 
+	@return:
+	 None
+	*/
+	func addScore() {
+		
+	}
 }
 
